e3ef1b1c4ca74d16a0c4c3d1618203de1138359e
Dave Megarry <dave@poweredbypixiedust.uk>
2017-12-22 23:17:54 +0000

    Genres created and added to Movie

--- Pidly/Models/Movie.cs
+++ Pidly/Models/Movie.cs
@@ -9,5 +9,10 @@ namespace Pidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public byte Stock { get; set; }
     }
 }
