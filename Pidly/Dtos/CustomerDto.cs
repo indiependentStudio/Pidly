d777ab72de61e5ee40c275641ee209285540bfd5
Dave Megarry <dave@poweredbypixiedust.uk>
2018-01-14 00:06:21 +0000

    Movies API completed

---
+++ Pidly/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        
+        [Required(ErrorMessage = "Please select a Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
---
+++ Pidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Pidly.Models;
+
+namespace Pidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+        
+        [Required]
+        [Range(1, 20)]
+        public byte Stock { get; set; }
+    }
+}
